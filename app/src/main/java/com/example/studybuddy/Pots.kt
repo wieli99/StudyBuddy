752260d763175abf7a8d8eac6098edb1bc5fde23
Florian Wieland <99wieli@gmail.com>
2020-01-20 19:39:21 +0100

    Added Pots design and Activity

---
+++ app/src/main/java/com/example/studybuddy/Pots.kt
@@ -0,0 +1,12 @@
+package com.example.studybuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pots : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pots)
+    }
+}
