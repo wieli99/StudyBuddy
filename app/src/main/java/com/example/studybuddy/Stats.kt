221897da2b9f319d724cb231fafe90ac0c899aec
Florian Wieland <99wieli@gmail.com>
2020-01-20 21:37:10 +0100

    Fixed divide-by-zero error

--- app/src/main/java/com/example/studybuddy/Stats.kt
+++ app/src/main/java/com/example/studybuddy/Stats.kt
@@ -30,13 +30,13 @@ class Stats : AppCompatActivity() {
         totalTime.setText((prefsMain.getInt("TotalMoney", 0) * 10 / 60).toString() + " min") //Calculates time based in money
         totalSessions.setText((prefsMain.getInt("TotalSessions", 0)).toString() + " sessions")
         totalMoney.setText((prefsMain.getInt("TotalMoney", 0)).toString() + "c")
-        averageSessions.setText((prefsMain.getInt("TotalMoney", 0) * 10 / 60 / prefsMain.getInt("TotalSessions", 0)).toString() + " min")
+        averageSessions.setText((prefsMain.getInt("TotalMoney", 0) * 10 / 60 / prefsMain.getInt("TotalSessions", 1)).toString() + " min")
         totalItems.setText((prefsMain.getInt("TotalItems", 0)).toString() + " items")
 
         Log.i("TotalTime", (prefsMain.getInt("TotalMoney", 0) * 10 / 60).toString() + " min")
         Log.i("TotalSessions", (prefsMain.getInt("TotalSessions", 0)).toString() + " sessions")
         Log.i("TotalMoney", (prefsMain.getInt("TotalMoney", 0)).toString() + "c")
-        Log.i("AvgSession", (prefsMain.getInt("TotalMoney", 0) * 10 / 60 / prefsMain.getInt("TotalSessions", 0)).toString() + " min")
+        Log.i("AvgSession", (prefsMain.getInt("TotalMoney", 0) * 10 / 60 / prefsMain.getInt("TotalSessions", 1)).toString() + " min")
         Log.i("TotalItems", (prefsMain.getInt("TODO", 0) * 10).toString() + " items")
     }
 }
