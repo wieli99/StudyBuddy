bd357304d2adac748e648b1c18b222e77ca9501f
Florian Wieland <99wieli@gmail.com>
2020-01-24 19:35:17 +0100

    Redesigned Settings; New color theme

--- app/src/main/java/com/wieliFritz/studybuddy/Settings.kt
+++ app/src/main/java/com/wieliFritz/studybuddy/Settings.kt
@@ -4,12 +4,11 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class Settings : AppCompatActivity() {
+    var current = 20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,26 +16,20 @@ class Settings : AppCompatActivity() {
 
         setBackground()
 
+        seekBarListeners()
+
+
         //Read the sharedPreferences money variable
         val prefsMain = getSharedPreferences("Main", 0)
-        val saveNameBtn = findViewById<Button>(R.id.saveNameSettingsButton)
-        val saveTimeBtn = findViewById<Button>(R.id.saveTimeSettingsButton)
 
         //Set saved value on startup
         findViewById<EditText>(R.id.editNameText).setText(prefsMain.getString("Name", "Frederick"))
-        findViewById<EditText>(R.id.editTimeText).setText(prefsMain.getInt("Time", 25).toString())
+    }
 
-        saveNameBtn.setOnClickListener{
-            storeName(prefsMain)
-            //To Hide Keyboard
-            findViewById<EditText>(R.id.editNameText).onEditorAction(EditorInfo.IME_ACTION_DONE);
-        }
 
-        saveTimeBtn.setOnClickListener{
-            storeTime(prefsMain)
-            //To Hide Keyboard
-            findViewById<EditText>(R.id.editNameText).onEditorAction(EditorInfo.IME_ACTION_DONE);
-        }
+    override fun onPause() {
+        super.onPause()
+        storeName(getSharedPreferences("Main", 0))
     }
 
 
@@ -45,16 +38,40 @@ class Settings : AppCompatActivity() {
             putString("Name", findViewById<EditText>(R.id.editNameText).text.toString())
             apply()
         }
-        Toast.makeText(this@Settings, getString(R.string.setting_saved_name_toast), Toast.LENGTH_SHORT).show()
     }
 
 
     fun storeTime(sharedPref: SharedPreferences){
         with (sharedPref.edit()) {
-            putInt("Time", findViewById<EditText>(R.id.editTimeText).text.toString().toInt()) //Seems stupid... but this is the way
+            putInt("Time", current)
             apply()
         }
-        Toast.makeText(this@Settings, getString(R.string.setting_saved_time_toast), Toast.LENGTH_SHORT).show()
+    }
+
+
+    fun seekBarListeners(){
+        val seekBarValue = findViewById<TextView>(R.id.seekBarValue)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val prefs = getSharedPreferences("Main", 0)
+        current = prefs.getInt("Time", 25)
+
+        seekBar.setProgress(current)
+        seekBarValue.setText(current.toString() + getString(R.string.stats_min))
+
+        seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                current = progress + 5 //5 = min
+                seekBarValue.setText(current.toString() + getString(R.string.stats_min))
+                storeTime(prefs)
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+            }
+        })
     }
 
 
